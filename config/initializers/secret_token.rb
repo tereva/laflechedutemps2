4f437db0245c7b26eda086cbb092d8ce5628b23d
tev <tvaillard@gmail.com>
2015-01-31 20:23:43 +0100

    Krono V0 : Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Krono::Application.config.secret_token = '35f6c04f9d250f405e5c6316170745b72447acc9eec2b0e29a10fcbffd2ac64b8b86c29db7471954e7dbcd546a4009b81521dcfa3016312dcb55749942e07770'
