3e21278b0b8a5ffe492838579687b3c9e4ae37ef
tev <tvaillard@gmail.com>
2016-02-15 20:15:03 +0100

    V0 bug frise bis

--- app/models/history.rb
+++ app/models/history.rb
@@ -38,8 +38,8 @@ public
 
 def prepareData(color, path, action)
 	tmp=[]
- 	events = self.events.select("title, start, end, durationEvent, description, place, linked_history_id")
-  	events.each do |event|
+ 	#events = self.events.select("title, start, end, durationEvent, description, place, linked_history_id")
+  	self.events.each do |event|
   		link = (event.linked_history_id ?  path+event.linked_history_id.to_s+action : nil) 
 	    tmp.push({:title => event.title, :start => event.start, :end => event.end,:place => event.place,
 	    :durationEvent => event.durationEvent, :textColor=> color, :link => link, :description => event.description})
